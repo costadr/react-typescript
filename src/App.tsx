fb12e3ffa57e10b8820cb59fbdb373b70e6ff7a0
Diogo Ricardo da Costa <diogo_cost@hotmail.com>
2023-08-09 17:20:58 -0300

    creating a drawer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { BrowserRouter } from 'react-router-dom';
 import { AppRoutes } from './routes';
 import { AppThemeProvider } from './shared/contexts';
+import { MenuLateral } from './shared/components';
 
 export const App = () => {
   return (
     <AppThemeProvider>
       <BrowserRouter>
-        <AppRoutes />
+      
+        <MenuLateral>
+          <AppRoutes />
+        </MenuLateral>
+
       </BrowserRouter>
     </AppThemeProvider>
   );
