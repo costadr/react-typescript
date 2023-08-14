5de1e50e8901977e0a742c3c6c611fdf88af240c
Diogo Ricardo da Costa <diogo_cost@hotmail.com>
2023-08-14 16:43:24 -0300

    useDebounce - custom hook to prevent unnecessary queries

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -4,33 +4,41 @@ import { useSearchParams } from 'react-router-dom';
 import { PessoasService } from '../../shared/services/api/pessoas/PessoasService';
 import { FerramentasDaListagem } from '../../shared/components';
 import { LayoutBaseDePagina } from '../../shared/layouts';
+import { useDebounce } from '../../shared/hooks';
+
 
 export const ListagemDePessoas: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const { debounce } = useDebounce(3000, false);
 
   const busca = useMemo(() => {
     return searchParams.get('busca') || '';
   }, [searchParams]);
 
-  useEffect(() => {
-    PessoasService.getAll(1, busca)
-      .then((result) => {
-        if (result instanceof Error) {
-          alert(result.message);
-        } else {
-          console.log(result);
-        }
-      });
-  },[busca]);
 
-  return(
-    <LayoutBaseDePagina 
+  useEffect(() => {
+
+    debounce(() => {
+      PessoasService.getAll(1, busca)
+        .then((result) => {
+          if (result instanceof Error) {
+            alert(result.message);
+          } else {
+            console.log(result);
+          }
+        });
+    });
+  }, [busca]);
+
+
+  return (
+    <LayoutBaseDePagina
       titulo='Listagem de pessoas'
       barraDeFerramentas={
-        <FerramentasDaListagem 
+        <FerramentasDaListagem
           mostrarInputBusca
-          textoBotaoNovo='Nova'
           textoDaBusca={busca}
+          textoBotaoNovo='Nova'
           aoMudarTextoDeBusca={texto => setSearchParams({ busca: texto }, { replace: true })}
         />
       }
