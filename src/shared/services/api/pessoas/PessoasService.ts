5aae9b610d3c2634de3f1df45ecdc0f1f4b3dfdd
Diogo Ricardo da Costa <diogo_cost@hotmail.com>
2023-08-14 17:39:14 -0300

    adding a table to the list of people

--- src/shared/services/api/pessoas/PessoasService.ts
+++ src/shared/services/api/pessoas/PessoasService.ts
@@ -2,14 +2,14 @@ import { Environment } from '../../../environment';
 import { Api } from '../axios-config';
 
 
-interface IListagemPessoa {
+export interface IListagemPessoa {
   id: number;
   email: string;
   cidadeId: number;
   nomeCompleto: string;
 }
 
-interface IDetalhePessoa {
+export interface IDetalhePessoa {
   id: number;
   email: string;
   cidadeId: number;
@@ -24,6 +24,7 @@ type TPessoasComTotalCount = {
 const getAll = async (page = 1, filter = ''): Promise<TPessoasComTotalCount | Error> => {
   try {
     const urlRelativa = `/pessoas?_page=${page}&_limit=${Environment.LIMITE_DE_LINHAS}&nomeCompleto_like=${filter}`;
+
     const { data, headers } = await Api.get(urlRelativa);
 
     if (data) {
@@ -32,11 +33,11 @@ const getAll = async (page = 1, filter = ''): Promise<TPessoasComTotalCount | Er
         totalCount: Number(headers['x-total-count'] || Environment.LIMITE_DE_LINHAS),
       };
     }
-    return new Error('Erro ao listar os registros');
 
+    return new Error('Erro ao listar os registros.');
   } catch (error) {
     console.error(error);
-    return new Error((error as { message: string }).message || 'Erro ao listar os registros');
+    return new Error((error as { message: string }).message || 'Erro ao listar os registros.');
   }
 };
 
@@ -47,11 +48,11 @@ const getById = async (id: number): Promise<IDetalhePessoa | Error> => {
     if (data) {
       return data;
     }
-    return new Error('Erro ao consultar o registro');
 
+    return new Error('Erro ao consultar o registro.');
   } catch (error) {
     console.error(error);
-    return new Error((error as { message: string }).message || 'Erro ao consultar o registro');
+    return new Error((error as { message: string }).message || 'Erro ao consultar o registro.');
   }
 };
 
@@ -62,11 +63,11 @@ const create = async (dados: Omit<IDetalhePessoa, 'id'>): Promise<number | Error
     if (data) {
       return data.id;
     }
-    return new Error('Erro ao criar o registro');
 
+    return new Error('Erro ao criar o registro.');
   } catch (error) {
     console.error(error);
-    return new Error((error as { message: string }).message || 'Erro ao criar o registro');
+    return new Error((error as { message: string }).message || 'Erro ao criar o registro.');
   }
 };
 
@@ -75,7 +76,7 @@ const updateById = async (id: number, dados: IDetalhePessoa): Promise<void | Err
     await Api.put(`/pessoas/${id}`, dados);
   } catch (error) {
     console.error(error);
-    return new Error((error as { message: string }).message || 'Erro ao atualizar o registro');
+    return new Error((error as { message: string }).message || 'Erro ao atualizar o registro.');
   }
 };
 
@@ -84,15 +85,15 @@ const deleteById = async (id: number): Promise<void | Error> => {
     await Api.delete(`/pessoas/${id}`);
   } catch (error) {
     console.error(error);
-    return new Error((error as { message: string }).message || 'Erro ao apagar o registro');
+    return new Error((error as { message: string }).message || 'Erro ao apagar o registro.');
   }
 };
 
 
 export const PessoasService = {
   getAll,
-  getById,
   create,
+  getById,
   updateById,
   deleteById,
 };
